f0fa83442d819270fc166c19fc9945f0d2cf7664
Allan2024 <allanjoseph009@gmail.com>
2021-09-20 22:40:36 +0530

    ADD

---
+++ src/basics.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class basics {
+    public static void main(String[] args){
+        String diable= "jambu";
+        System.out.println(diable.indexOf("bu"));
+    }
+
+}
